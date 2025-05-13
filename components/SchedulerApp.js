c8f6b53050be6f7f7a640e16fcdc113bfe31dd39
GUERINANDSONS <chris@guerinandsons.com>
2025-05-12 21:20:42 -0700

    Update SchedulerApp.js

--- components/SchedulerApp.js
+++ components/SchedulerApp.js
@@ -1,1 +1,17 @@
-// updated logic with 1-day offset recalculation
+const recalculateSchedule = () => {
+  const updated = [...projects[projectId]].map(task => ({ ...task })); // deep copy
+  for (let i = 0; i < updated.length; i++) {
+    if (i === 0) {
+      const start = new Date(updated[i].startDate);
+      updated[i].endDate = format(addDays(start, updated[i].duration), 'yyyy-MM-dd');
+    } else {
+      const prevEnd = new Date(updated[i - 1].endDate);
+      const start = addDays(prevEnd, 1);
+      updated[i].startDate = format(start, 'yyyy-MM-dd');
+      updated[i].endDate = format(addDays(start, updated[i].duration), 'yyyy-MM-dd');
+    }
+  }
+
+  const newProjects = { ...projects, [projectId]: updated };
+  setProjects(newProjects);
+};
