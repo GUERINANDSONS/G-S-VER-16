3f33d7288fe55655f03e84b05786ff6b79deef04
GUERINANDSONS <chris@guerinandsons.com>
2025-05-12 21:32:38 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,11 @@
-// entry file
+import dynamic from 'next/dynamic';
+const Scheduler = dynamic(() => import('../components/SchedulerApp'), { ssr: false });
+
+export default function Home() {
+  return (
+    <main style={{ padding: '1rem' }}>
+      <h1>Custom Home Scheduler</h1>
+      <Scheduler />
+    </main>
+  );
+}
